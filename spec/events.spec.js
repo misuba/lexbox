4f97b1a8ca5be90535bff8de21caa4b4baeb4907
mike sugarbaker <misuba@gmail.com>
2012-11-23 09:00:52 -0800

    get started on that history-logging business

---
+++ spec/events.spec.js
@@ -0,0 +1,18 @@
+describe("events", function() {
+  var mongoose = require('mongoose'),
+      db = mongoose.connect("mongodb://localhost/lexbox-test"),
+      LText = db.model('LText', require('../models/texts').schema);
+      LBox = db.model('LText', require('../models/boxes').schema);
+      LEvent = db.model('LText', require('../models/events').schema);
+
+  it("logs text creation");
+
+  it("logs box creation");
+
+  it("logs box insertion of a group of texts");
+
+  it("logs box removal of a group of texts");
+
+  it("gets last 50 historic events");
+
+});
